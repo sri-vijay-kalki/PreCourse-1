5f6061e8e8864781fbc609bce001a5f8c1f8217a
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-05-13 21:22:36 -0400

    [ADD]: Excercise_2-add push, pop, peek, is empty functionality for stack implement using linkedlist

--- Exercise_2.java
+++ Exercise_2.java
@@ -1,3 +1,13 @@
+/**
+ *
+ * Time Complexity:
+ * O(1) --> isEmpty(), push(), pop(), peek()
+ *
+ * Space Complexity:
+ * O(1) --> isEmpty(), push(), pop(), peek()
+ *
+ */
+
 public class StackAsLinkedList { 
   
     StackNode root; 
@@ -9,6 +19,7 @@ public class StackAsLinkedList {
         StackNode(int data) 
         { 
             //Constructor here 
+            this.data = data;
         } 
     } 
     
@@ -16,11 +27,17 @@ public class StackAsLinkedList {
     public boolean isEmpty() 
     { 
         //Write your code here for the condition if stack is empty. 
+//        If root Node is null then the stack is empty
+        return root == null;
     } 
   
     public void push(int data) 
     { 
         //Write code to push data to the stack. 
+//        Add new Node to the front and make the new node head
+        StackNode newNode = new StackNode(data);
+        newNode.next = this.root;
+        this.root = newNode;
     } 
   
     public int pop() 
@@ -28,11 +45,23 @@ public class StackAsLinkedList {
 	//If Stack Empty Return 0 and print "Stack Underflow"
         //Write code to pop the topmost element of stack.
 	//Also return the popped element 
+        if(isEmpty()) {
+            System.out.println("Stack Underflow");
+            return 0;
+        }
+//        return head value and make head.next as head
+        int topVal  = root.data;
+        root = root.next;
+        return topVal;
     } 
   
     public int peek() 
     { 
         //Write code to just return the topmost element without removing it.
+//        return head.data if stack is not empty else return 0
+        if(isEmpty())
+            return 0;
+        return root.data;
     } 
   
 	//Driver code
